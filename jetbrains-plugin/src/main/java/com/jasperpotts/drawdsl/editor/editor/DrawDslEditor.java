d906dfe9ce915ae09f13e1c55aff987bd237b005
Jasper Potts <1466205+jasperpotts@users.noreply.github.com>
2026-03-18 12:15:13 -0700

    work on plugin ui
    Signed-off-by: Jasper Potts <1466205+jasperpotts@users.noreply.github.com>

--- jetbrains-plugin/src/main/java/com/jasperpotts/drawdsl/editor/editor/DrawDslEditor.java
+++ jetbrains-plugin/src/main/java/com/jasperpotts/drawdsl/editor/editor/DrawDslEditor.java
@@ -1,5 +1,9 @@
 package com.jasperpotts.drawdsl.editor.editor;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorState;
 import com.intellij.openapi.project.Project;
@@ -16,11 +20,16 @@ import javax.swing.*;
 import java.beans.PropertyChangeListener;
 
 public class DrawDslEditor extends UserDataHolderBase implements FileEditor {
+    private static final String EMPTY_DIAGRAM =
+            "<mxGraphModel><root><mxCell id=\"0\"/><mxCell id=\"1\" parent=\"0\"/></root></mxGraphModel>";
+
+    private final Project project;
     private final VirtualFile file;
     private final JSplitPane mainPanel;
     private final DrawDslBrowserPanel browserPanel;
 
-    public DrawDslEditor(@SuppressWarnings("unused") @NotNull Project project, @NotNull VirtualFile file) {
+    public DrawDslEditor(@NotNull Project project, @NotNull VirtualFile file) {
+        this.project = project;
         this.file = file;
 
         browserPanel = new DrawDslBrowserPanel();
@@ -31,6 +40,37 @@ public class DrawDslEditor extends UserDataHolderBase implements FileEditor {
         mainPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, browserPanel, sidePanel);
         mainPanel.setResizeWeight(0.75);
         mainPanel.setDividerSize(4);
+
+        // Load file content into the browser
+        Document document = FileDocumentManager.getInstance().getDocument(file);
+        if (document != null) {
+            browserPanel.loadDiagramXml(extractXml(document.getText()));
+        }
+
+        // On diagram change: write back to the document so Ctrl+S / auto-save work normally
+        browserPanel.setDiagramChangeListener(xml -> ApplicationManager.getApplication().invokeLater(() -> {
+            Document doc = FileDocumentManager.getInstance().getDocument(file);
+            if (doc != null && doc.isWritable()) {
+                WriteCommandAction.runWriteCommandAction(
+                        project,
+                        DrawDslBundle.message("editor.command.update"),
+                        null,
+                        () -> doc.setText(xml)
+                );
+            }
+        }));
+    }
+
+    /**
+     * Extracts the mxGraphModel XML from file content.
+     * Handles both raw mxGraphModel XML and SVG files with embedded diagram data.
+     */
+    private static String extractXml(String content) {
+        int idx = content.indexOf("<mxGraphModel");
+        if (idx >= 0) {
+            return content.substring(idx);
+        }
+        return EMPTY_DIAGRAM;
     }
 
     @Override
@@ -54,7 +94,8 @@ public class DrawDslEditor extends UserDataHolderBase implements FileEditor {
 
     @Override
     public boolean isModified() {
-        return false;
+        Document document = FileDocumentManager.getInstance().getDocument(file);
+        return document != null && FileDocumentManager.getInstance().isDocumentUnsaved(document);
     }
 
     @Override
